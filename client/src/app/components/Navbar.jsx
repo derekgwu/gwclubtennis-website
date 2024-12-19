f5b13aacef18b5495140c787412729426a8f4084
Derek Chen <dchen36@gwmail.gwu.edu>
2024-12-19 16:12:00 -0500

    started work on homepage

---
+++ client/src/app/components/Navbar.jsx
@@ -0,0 +1,20 @@
+
+import "./Navbar.css";
+
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+        <div className="nav-link">
+            <h2>Home</h2>
+        </div>
+        <div className="nav-link">
+            <h2>Roster</h2>
+        </div>
+        <div className="nav-link">
+            <h2>Socials</h2>
+        </div>
+      
+    </div>
+  );
+}
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,35 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import "./home.css";
+import Navbar from "./components/Navbar";
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <p>gw club tennis</p>
+    <div>
+      <Navbar/>
+      <div className="main-header-div">
+
+        <div className="main-text-side">
+
+          <div className="main-text">
+            <h2>GW CLUB TENNIS</h2>
+            <p>GW Club Tennis was founded in 2005. It is a coed team that 
+              competes at tournaments in and around the DMV. The club centers 
+              around players with a history of competitive play who want to continue 
+              developing their game throughout college.</p>
+          </div>
+
+        </div>
+
+        <div className="main-image-side">
+          <div className="dot-grid"></div>
+        </div>
+
+      </div>
+      <div className="content-area1">
+
+      </div>
     </div>
   );
 }
